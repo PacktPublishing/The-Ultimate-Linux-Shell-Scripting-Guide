8fe5d1c734e3a53a3cb8857cc2034cfe378e51c0
donniet4 <55449545+donniet4@users.noreply.github.com>
2023-10-04 16:29:11 -0400

    Add files via upload

---
+++ Chapter 8/UID-0_check.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+os=$(uname) 
+quantity=$(cut -f3 -d: /etc/passwd | grep -w 0 | wc -l) 
+if [ $os == Linux ]; then
+        if [ $quantity -gt 1 ]; then
+       
+                echo "CRITICAL.  There are $quantity accounts with UID 0." 
+                exit 2 
+        else 
+                echo "OKAY.  There is only one account with UID 0." 
+                exit 1
+        fi 
+elif [ $os == Darwin ]; then
+        if [ $quantity -gt 1 ]; then
+                echo "CRITICAL.  There are $quantity accounts with UID 0." 
+                exit 2 
+        else 
+                echo "OKAY.  There is only one account with UID 0." 
+                exit 1
+        fi 
+elif [ $os == SunOS ]; then
+        if [ $quantity -gt 1 ]; then
+                echo "CRITICAL.  There are $quantity accounts with UID 0." 
+                exit 2 
+        else 
+                echo "OKAY.  There is only one account with UID 0." 
+                exit 1
+        fi 
+elif [ $os == FreeBSD ]; then
+        if [ $quantity -gt 2 ] 
+        then 
+                echo "CRITICAL.  There are $quantity accounts with UID 0." 
+                        exit 2 
+        else 
+                        echo "OKAY.  There are only two accounts with UID 0." 
+                        exit 1 
+        fi 
+else
+	echo "I don't know this $os operating system."
+fi
