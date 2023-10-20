3d01e5cb5af03bf032f36ae12f8e749507e51582
donniet4 <55449545+donniet4@users.noreply.github.com>
2023-10-20 14:43:35 -0400

    Add files via upload

---
+++ Chapter 8/netchk.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+if [[ $# -eq 0 ]]; then
+	site="google.com"
+else
+	site="$1"
+fi
+
+ping -c 2 $site > /dev/null
+if [[ $? != 0 ]]; then
+	echo $(date +%F) . . . Network Failure!
+	logger "Could not reach $site."
+else
+	echo $(date +%F) . . . Success!
+	logger "$site is reachable."
+fi
