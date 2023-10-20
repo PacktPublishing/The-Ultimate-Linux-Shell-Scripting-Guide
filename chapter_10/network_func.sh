ff123b1439dcff49b852212285f17dc613adf97e
donniet4 <55449545+donniet4@users.noreply.github.com>
2023-10-20 15:13:22 -0400

    Add files via upload

--- chapter_10/network_func.sh
+++ chapter_10/network_func.sh
@@ -1,1 +1,21 @@
+#!/bin/sh
 
+network() {
+if [[ $# -eq 0 ]]; then
+        site="google.com"
+else
+        site="$1"
+fi
+ping -c 2 $site > /dev/null
+if [[ $? != 0 ]]; then
+        echo $(date +%F) . . . Network Failure!
+        logger "Could not reach $site."
+else
+        echo $(date +%F) . . . Success!
+        logger "$site is reachable."
+fi
+}
+
+network
+network www.civicsandpolitics.com
+network donnie.com
