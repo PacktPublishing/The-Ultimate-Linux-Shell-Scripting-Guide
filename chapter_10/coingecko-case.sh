e0c726c29d6172a16ac3ab5ef7b7246defc1a1be
donniet4 <55449545+donniet4@users.noreply.github.com>
2023-10-27 14:20:38 -0400

    Add files via upload

---
+++ chapter_10/coingecko-case.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+gecko_api() {
+	coin=$1
+	currency=$2
+
+case "$1" in
+
+"")
+	echo "Usage:"
+	echo "./coingecko.sh list"
+	echo "./coingecko.sh currencies"
+	echo "./coingecko.sh coin_name(s) currency"
+	;;
+	
+list)
+	curl -X 'GET' \
+  "https://api.coingecko.com/api/v3/coins/list?include_platform=true" \
+  -H 'accept: application/json' | jq | tee coinlist.txt
+	  ;;
+
+currency)
+       curl -X 'GET' \
+  'https://api.coingecko.com/api/v3/simple/supported_vs_currencies' \
+  -H 'accept: application/json' | jq | tee currency_list.txt
+	 ;;
+
+ *)
+	curl -X 'GET' \
+  "https://api.coingecko.com/api/v3/simple/price?ids=$coin&vs_currencies=$2&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true&precision=true" \
+  -H 'accept: application/json' | jq
+	;;
+esac
+}
+gecko_api $1 $2
