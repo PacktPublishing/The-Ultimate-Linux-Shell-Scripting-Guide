a2fa6d07e74c6a1aafe6d735e2f8f2694db6c0f7
donniet4 <55449545+donniet4@users.noreply.github.com>
2023-11-02 14:05:39 -0400

    Add files via upload

---
+++ chapter_11/report_os.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+file=$1
+
+sed -i '1d'  $file
+
+echo "The market share for Windows is $(grep 'Win' $file | cut -d, -f2 | grep . | paste -s -d+ | bc)%." > report_for_$(date +%F).txt
+
+echo "The market share for macOS is $(grep 'OS X' $file | cut -d, -f2)%." >> report_for_$(date +%F).txt
+
+echo "The market share for Linux is $(grep 'Linux' $file | cut -d, -f2)%." >> report_for_$(date +%F).txt
+
+echo "The market share for Chrome is $(grep 'Chrome OS' $file | cut -d, -f2)%." >> report_for_$(date +%F).txt
+
+echo "The market share for Unknown is $(grep 'Unknown' $file | cut -d, -f2)%." >> report_for_$(date +%F).txt
+
+echo "The market share for Others is $(grep 'Other' $file | cut -d, -f2)%." >> report_for_$(date +%F).txt
