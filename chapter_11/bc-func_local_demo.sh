f261a84d2d96dee1291a7d2fe910ac2ae8577db5
donniet4 <55449545+donniet4@users.noreply.github.com>
2023-11-02 13:15:43 -0400

    Add files via upload

--- chapter_11/bc-func_local_demo.sh
+++ chapter_11/bc-func_local_demo.sh
@@ -1,40 +1,48 @@
 #!/bin/bash
-. /usr/local/lib/baseconv.lib
+. /usr/local/lib/baseconv_local.lib
 until [ "$choice" = "q" ]; do
 	echo "Choose your desired function from the following list: "
 	echo "For hex to decimal, press \"1\"."
 	echo "For decimal to hex, press \"2\"."
 	echo "For octal to hex, press \"3\"."
 	echo "For hex to octal, press \"4\"."
+	echo "For decial to binary, press \"5\"."
 	echo "To quit, press \"q\"."
 
 	read choice
 	case $choice in
 		1) echo "Enter the hex number that you would like to convert. "
 			read input_num
-			h2d
-			echo $h2dnum
+			result=$(h2d)
+			echo $result
 			echo
 			echo
 			;;
 		2) echo "Enter the decimal number that you would like to convert. "
  			read input_num
-			d2h
-			echo $d2hnum
+			result=$(d2h)
+			echo $result
 			echo
 			echo
 			;;
 		3) echo "Enter the octal number that you would like to convert. "
 			read input_num
-			o2h
-			echo $o2hnum
+			result=$(o2h)
+			echo $result
 			echo
 			echo
 			;;
 		4) echo "Enter the hex number that you would like to convert. "
 			read input_num
-			h2o
-			echo $h2onum
+			result=$(h2o)
+			echo $result
+			echo
+			echo
+			;;
+		5) echo "Enter the decimal number that you would like to convert to binary. "
+			read input_num
+			result=$(d2b)
+			echo $result
 			echo
 			echo
 			;;
