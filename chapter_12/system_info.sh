936d53fc1feece83c586548330ef64ac0ced61b8
donniet4 <55449545+donniet4@users.noreply.github.com>
2023-11-10 16:31:49 -0500

    Add files via upload

---
+++ chapter_12/system_info.sh
@@ -0,0 +1,31 @@
+#!/bin/bash 
+
+# sysinfo_page - A script to produce an HTML file 
+. /usr/local/lib/sysinfo.lib 
+title="System Information for $HOSTNAME" 
+right_now=$(date +"%x %r %Z") 
+time_stamp="Updated on $right_now by $USER" 
+
+cat <<- _system-info_ > sysinfo.html
+    <html> 
+    <head> 
+        <title> 
+        $title 
+        </title> 
+    </head> 
+
+    <body> 
+    <h1>$title</h1> 
+    <p>$time_stamp</p> 
+    $(system_info) 
+    $(show_uptime) 
+    $(drive_space) 
+    $(home_space) 
+    $(open_files_root)
+    $(open_files_users)
+    </body> 
+    </html> 
+_system-info_
+pandoc -o sysinfo.pdf sysinfo.html
+rm sysinfo.html
+exit
