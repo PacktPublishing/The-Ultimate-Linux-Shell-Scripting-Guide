3f742debb4c00b84481e154d5a5da07deda23888
donniet4 <55449545+donniet4@users.noreply.github.com>
2024-02-25 15:51:42 -0500

    Add files via upload

--- chapter_13/rename_extension.sh
+++ chapter_13/rename_extension.sh
@@ -1,1 +1,4 @@
-
+#!/bin/bash
+for file in *.JPG; do
+	mv -- "$file" "${file%.JPG}.jpg"
+done
---
+++ chapter_13/rename_extension2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+for file in *.JPG; do
+	mv -- $file "$(basename -- "$file" .JPG).jpg"
+done
