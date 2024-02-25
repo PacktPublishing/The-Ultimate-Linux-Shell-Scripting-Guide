15c2e44df0b729b29ed665d4950f6e9943b04529
donniet4 <55449545+donniet4@users.noreply.github.com>
2024-02-25 16:10:30 -0500

    Add files via upload

--- Chapter_16/yad-image-resize2.sh
+++ Chapter_16/yad-image-resize2.sh
@@ -9,6 +9,6 @@ dialog=$(yad --title "Image Resize" --form --field="Resize parameter" --field="Q
 size=$(echo $dialog | awk 'BEGIN {FS="|"} {print $1}')
 quality=$(echo $dialog | awk 'BEGIN {FS="|"} {print $2}')
 
-convert "$imageFile" -resize "$size" -quality $quality "$quality" "$imageFile" -delete 1 "$newfilename"_resized"$suffix"
+convert -resize "$size" -quality "$quality" "$imageFile" -delete 1 "$newfilename"_resized"$suffix"
 
 yad --title="Status" --width=300  --button="OK:0"  --text="All done. Yay!"
