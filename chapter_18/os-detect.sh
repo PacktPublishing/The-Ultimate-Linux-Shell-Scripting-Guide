b25fe0a01e58fa03c352615f302f5b95b5ebc801
donniet4 <55449545+donniet4@users.noreply.github.com>
2024-05-06 13:19:01 -0400

    Add files via upload

--- chapter_18/os-detect.sh
+++ chapter_18/os-detect.sh
@@ -1,1 +1,13 @@
-
+#!/bin/bash
+ttl_temp=$(ping -c1 $1 | head -2 | tail -1 | cut -d= -f3)
+ttl=$(echo $ttl_temp | cut -d" " -f1)
+echo "The TTL value is $ttl."
+if [[ $ttl == 64 ]]; then
+	 echo "A TTL of $ttl indicates either a Linux, BSD, or macOS operating system."
+elif [[ $ttl == 128 ]]; then
+       	echo "A TTL of $ttl indicates a Windows operating system."
+elif [[ $ttl == 255 ]] ; then
+       	echo "A TTL of $ttl indicates a Solaris/OpenIndiana operating system."
+else
+	echo "There was no recognized TTL value."
+fi
