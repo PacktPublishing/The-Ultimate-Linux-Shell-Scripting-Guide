ebed7196f2098c2e0409996bf480643ef41c4905
donniet4 <55449545+donniet4@users.noreply.github.com>
2024-05-29 22:55:01 -0400

    Add files via upload

---
+++ Chapter_19/system_info_posix.sh
@@ -0,0 +1,35 @@
+#!/bin/sh 
+
+# sysinfo_page - A script to produce an HTML file 
+. /usr/local/lib/sysinfo_posix.lib 
+title="System Information for $(hostname)" 
+right_now=$(date +"%x %r %Z") 
+time_stamp="Updated on $right_now by $USER" 
+
+cat <<- _system-info_ > sysinfo.html
+    <html> 
+    <head> 
+        <title> 
+        $title 
+        </title> 
+    </head> 
+
+    <body> 
+    <h1>$title</h1> 
+    <p>$time_stamp</p> 
+    $(system_info) 
+    $(show_uptime) 
+    $(drive_space) 
+    $(home_space) 
+    $(open_files_root)
+    $(open_files_users)
+    $(VSZ_info)
+    </body> 
+    </html> 
+_system-info_
+
+if [ -f /usr/local/bin/pandoc ] || [ -f /usr/bin/pandoc ]; then
+	pandoc -o sysinfo.pdf sysinfo.html
+	rm sysinfo.html
+fi
+exit
