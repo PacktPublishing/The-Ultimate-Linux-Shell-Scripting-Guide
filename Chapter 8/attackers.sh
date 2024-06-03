f0a8e473ed1e9ac0cd398147e5d45311520e1ab9
donniet4 <55449545+donniet4@users.noreply.github.com>
2024-06-03 15:04:28 -0400

    Add files via upload

---
+++ Chapter 8/attackers.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+badips=$(cat /opt/scripts/banned.txt) 
+declare -a attackers 
+attackers=( $badips ) 
+echo "Here is the complete list: " 
+echo ${attackers[@]} 
+echo 
+echo "Let us now count the items in the list."
+elements=${#attackers[*]} 
+echo "There are $elements IP addresses in the list." 
+echo 
+echo "attackers[2] is ${attackers[2]}, which is the third address in the list." 
+exit
