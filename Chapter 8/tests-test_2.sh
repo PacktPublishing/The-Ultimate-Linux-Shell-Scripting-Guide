bd9cebc8df1b4c793565c9917011e6666030ec60
donniet4 <55449545+donniet4@users.noreply.github.com>
2024-06-05 14:28:50 -0400

    Add files via upload

--- Chapter 8/tests-test_2.sh
+++ Chapter 8/tests-test_2.sh
@@ -2,9 +2,9 @@
 echo "We will now compare text strings."
 string1="abcd"
 string2="efgh"
-[[ $string1 > string2 ]] && echo "string1 comes after string2 alphabetically." || echo "string1 comes before string2 alphabetically."
+[[ $string1 > $string2 ]] && echo "string1 comes after string2 alphabetically." || echo "string1 comes before string2 alphabetically."
 echo
 echo "We will now compare numbers."
 num1=10
 num2=9
-[[ num1 -gt num2 ]] && echo "num1 is greater than num2." || echo "num1 is less than num2."
+[[ $num1 -gt $num2 ]] && echo "num1 is greater than num2." || echo "num1 is less than num2."
