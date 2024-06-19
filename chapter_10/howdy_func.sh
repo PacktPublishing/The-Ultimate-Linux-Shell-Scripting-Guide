3684bce35c516b6176f2730a9f8ab388c8f381a9
donniet4 <55449545+donniet4@users.noreply.github.com>
2024-06-19 16:25:03 -0400

    Add files via upload

---
+++ chapter_10/howdy_func.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+howdy () 
+{ 
+    echo "Howdy, world!";
+    echo "How's it going?"
+}
+
+howdy
+echo
+echo "Let's do some more stuff and then"
+echo "we'll call the function again."
+echo
+howdy
+
