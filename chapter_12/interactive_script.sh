a0d1f174b3f69e1fe67639d73ea08f3364290b43
donniet4 <55449545+donniet4@users.noreply.github.com>
2024-06-22 16:38:19 -0400

    Add files via upload

---
+++ chapter_12/interactive_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Hello. What is your name?"
+read $reply
+echo "What's your favorite operating system?"
+read $reply
+echo "How many cats to you have?"
+read $reply
+echo "How many dogs do you have?"
+read $reply
