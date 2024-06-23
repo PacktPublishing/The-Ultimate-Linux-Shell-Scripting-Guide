d5c5df661e34e2729378648d4bf78515852de5e5
donniet4 <55449545+donniet4@users.noreply.github.com>
2024-06-23 15:52:00 -0400

    Add files via upload

---
+++ chapter_12/here-doc1-tabs-dash.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cat <<- licensing   
+		This software is released under the GNU GPL 3 license.
+	Anyone who uses this software must respect the terms
+	of this license.
+
+   GNU GPL 3 is a license that aims to protect software freedom.
+
+licensing
